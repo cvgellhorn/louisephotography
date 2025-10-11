1205cd2a96731247871912d0302a3be600b8e585
Christoph von Gellhorn <gellborn@gmail.com>
2025-10-11 19:38:04 +1100

    update site URL in astro.config.mjs to reflect new domain for Louise Pillon Photography

--- astro.config.mjs
+++ astro.config.mjs
@@ -9,8 +9,7 @@ import icon from "astro-icon"; // https://www.astroicon.dev/guides/upgrade/v1/
 // https://astro.build/config
 export default defineConfig({
 	// output: "static",
-	site: "https://cvgellhorn.github.io",
-	base: "/louisephotography",
+	site: "https://louisepillon.com",
 	integrations: [
 		// example auto import component into blog post mdx files
 		AutoImport({
